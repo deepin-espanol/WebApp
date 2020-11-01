98ab9767b9c161ee10807ab66742e851be7cc86e
alvarosamudio <alvarosamudio@criptext.com>
2020-11-01 05:41:36 -0500

    update translate

---
+++ translations/WebApp_es_ES.ts
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../globaldefine.h" line="7"/>
+        <source>WebApp</source>
+        <translation>WebApp</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="52"/>
+        <source>Version</source>
+        <translation>Versión</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="60"/>
+        <source>Description: %1</source>
+        <translation>Descripción: %1</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="65"/>
+        <source>Enable CommandLineParser. Default is false.</source>
+        <translation>Habilite el CommandLineParser. El valor por defecto es falso.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="69"/>
+        <source>The Title of Application. Default is %1.</source>
+        <translation>El título de la aplicación. Por defecto es %1.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="75"/>
+        <source>The target URL. Default is Blank.</source>
+        <translation>La URL del destino Por defecto está en blanco.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="81"/>
+        <source>The Width of Application. Default is %1.</source>
+        <translation>La amplitud de la aplicación. Por defecto es %1.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="87"/>
+        <source>The Height of Application. Default is %1</source>
+        <translation>La altura de la aplicación. Por defecto es %1</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="93"/>
+        <source>The ICON of Application.</source>
+        <translation>El Icono de la aplicación.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="99"/>
+        <source>The Description of Application.</source>
+        <translation>La descripción de la aplicación.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="105"/>
+        <source>The Configuration file of Application.</source>
+        <translation>El archivo de configuración de la aplicación.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="111"/>
+        <source>The root path of the program web service.</source>
+        <translation>La ruta raíz del servicio web del programa.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="118"/>
+        <source>The port number of the program web service.</source>
+        <translation>El número de puerto del servicio web del programa.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="125"/>
+        <source>The ssl port number of the program web service.</source>
+        <translation>El número de puerto ssl del servicio web del programa.</translation>
+    </message>
+</context>
+<context>
+    <name>Widget</name>
+    <message>
+        <location filename="../widget.ui" line="14"/>
+        <source>MainPage</source>
+        <translation>PáginaPrincipal</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/WebApp_zh_CN.ts
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../main.cpp" line="52"/>
+        <source>Version</source>
+        <translation>版本</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="60"/>
+        <source>Description: %1</source>
+        <translation>描述: %1</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="65"/>
+        <source>Enable CommandLineParser. Default is false.</source>
+        <translation>启用参数解析方式. 默认顺序解析方式.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="69"/>
+        <source>The Title of Application. Default is %1.</source>
+        <translation>设置程序的运行标题. 默认是 %1.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="75"/>
+        <source>The target URL. Default is Blank.</source>
+        <translation>设置要打开的目标URL. 默认是空.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="81"/>
+        <source>The Width of Application. Default is %1.</source>
+        <translation>设置程序运行的宽度. 默认是 %1.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="87"/>
+        <source>The Height of Application. Default is %1</source>
+        <translation>设置程序运行的高度. 默认是 %1</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="93"/>
+        <source>The ICON of Application.</source>
+        <translation>设置程序运行的图标.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="99"/>
+        <source>The Description of Application.</source>
+        <translation>设置程序的描述信息.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="105"/>
+        <source>The Configuration file of Application.</source>
+        <translation>设置程序运行的配置文件.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="111"/>
+        <source>The root path of the program web service.</source>
+        <translation>设置内置WebServer的根路径.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="118"/>
+        <source>The port number of the program web service.</source>
+        <translation>设置内置WebServer的监听端口号.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="125"/>
+        <source>The ssl port number of the program web service.</source>
+        <translation>设置内置WebServer的SSL协议的监听端口号.</translation>
+    </message>
+    <message>
+        <location filename="../globaldefine.h" line="7"/>
+        <source>WebApp</source>
+        <translation>WebApp</translation>
+    </message>
+</context>
+<context>
+    <name>Widget</name>
+    <message>
+        <location filename="../widget.ui" line="14"/>
+        <source>MainPage</source>
+        <translation>主页</translation>
+    </message>
+</context>
+</TS>
